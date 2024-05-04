5f134d82cc89681095dfc29785c1d38979d89e32
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-04 20:55:26 +0530

    renamed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="scroll-smooth">{children}</body>
     </html>
   );
 }
