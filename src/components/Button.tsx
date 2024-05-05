169a6b0548c0e3434a4ed26954dabbf28f4b101e
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-05 16:44:16 +0530

    done with the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,10 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { useState } from "react";
+import '../styles/switch.css'
 
 export const Button = () => {
-  const [toggle, setToggle] = useState<boolean>(false);
-
   return (
-    <div className="flex justify-center">
-      <div
-        onClick={() => setToggle(!toggle)}
-        className={`flex items-center justify-${
-          toggle ? "end" : "start"
-        } h-6 w-12 cursor-pointer rounded-full p-[1px] border border-black ${
-          toggle ? "bg-white" : "bg-black"
-        }`}
-      >
-        <motion.div
-          className={`h-5 w-5 rounded-full ${toggle ? "bg-black" : "bg-white"}`}
-          layout
-          transition={{ type: "spring", stiffness: 700, damping: 30 }}
-        />
-      </div>
-    </div>
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round"></span>
+    </label>
   );
 };
