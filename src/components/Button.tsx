b71e4c087f8c5171ed6dddce4b8238d348fd744f
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-05 13:29:27 +0530

    created a switch button to toggle the theme

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,27 @@
-type Props = {};
-export const Button = ({}: Props) => {
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export const Button = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
   return (
-    <button>Theme</button>
+    <div className="flex justify-center">
+      <div
+        onClick={() => setToggle(!toggle)}
+        className={`flex items-center justify-${
+          toggle ? "end" : "start"
+        } h-6 w-12 cursor-pointer rounded-full p-[1px] border border-black ${
+          toggle ? "bg-white" : "bg-black"
+        }`}
+      >
+        <motion.div
+          className={`h-5 w-5 rounded-full ${toggle ? "bg-black" : "bg-white"}`}
+          layout
+          transition={{ type: "spring", stiffness: 700, damping: 30 }}
+        />
+      </div>
+    </div>
   );
 };
