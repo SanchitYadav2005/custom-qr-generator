8d9250eaa60a5386a27fbf7c99ea9c075b3b49a9
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-06 21:05:30 +0530

    created button for theme

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,27 @@
-import '../styles/switch.css'
+"use client";
 
+import "../styles/switch.css";
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
 export const Button = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
-    <label className="switch">
-      <input type="checkbox" />
-      <span className="slider round"></span>
-    </label>
+    <button
+      className="button"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? (
+        <SunIcon className="h-5 w-5 text-slate-800" />
+      ) : (
+        <MoonIcon className="h-5 w-5 text-slate-800" />
+      )}
+    </button>
   );
 };
