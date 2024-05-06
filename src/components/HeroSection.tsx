ae659cb37d873ac25fe7da86f85ae49ce01ed595
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-06 21:52:24 +0530

    seted up hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import facebook from "../../public/facebook.jpg";
+import youtube from "../../public/youtube.jpg";
+import twitter from "../../public/twitter.jpg";
+import '../styles/herosection.css';
+
+type Props = {};
+export const HeroSection = ({}: Props) => {
+  return (
+    <div className="hero-section">
+      <div className="text">
+        {" "}
+        Generate <span>custom QR codes</span> in seconds. <span>Simplify </span>connections, <span>amplify</span>
+        impact.
+      </div>
+      <div className="image-container">
+        <Image
+          src={facebook}
+          alt="facebook"
+          placeholder={"blur"}
+        />
+        <Image
+          src={twitter}
+          alt="twitter"
+          placeholder={"blur"}
+        />
+        <Image
+          src={youtube}
+          alt="youtube"
+          placeholder={"blur"}
+        />
+      </div>
+    </div>
+  );
+};
