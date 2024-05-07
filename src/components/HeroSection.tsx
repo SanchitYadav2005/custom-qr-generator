c18769889da3ac596bbaa760788eed333393f118
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-07 19:24:32 +0530

    working on the hero secriotn

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,27 +8,31 @@ type Props = {};
 export const HeroSection = ({}: Props) => {
   return (
     <div className="hero-section">
-      <div className="text">
-        {" "}
-        Generate <span>custom QR codes</span> in seconds. <span>Simplify </span>connections, <span>amplify</span>
-        impact.
-      </div>
+      
       <div className="image-container">
         <Image
           src={facebook}
           alt="facebook"
           placeholder={"blur"}
+          className="img"
         />
         <Image
           src={twitter}
           alt="twitter"
           placeholder={"blur"}
+          className="img"
         />
         <Image
           src={youtube}
           alt="youtube"
           placeholder={"blur"}
+          className="img"
         />
+      </div>
+      <div className="text">
+        {" "}
+        Generate <span>custom QR codes</span> in seconds. <span>Simplify </span>connections, <span>amplify </span>
+        impact.
       </div>
     </div>
   );
