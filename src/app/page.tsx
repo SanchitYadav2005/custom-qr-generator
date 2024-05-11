ce63b9633c10b113599e6b151e9e9b193703bc87
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-11 17:18:26 +0530

    added .env file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import { HeroSection } from "@/components/HeroSection";
+import { Form } from "@/components/Form";
 
 export default function Home() {
   return (
     <>
    <Navbar/>
    <HeroSection/>
+   <Form/>
    </>
   );
 }
