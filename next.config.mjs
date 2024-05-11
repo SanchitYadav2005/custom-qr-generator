504a980370b96439201e8a9c1660bf53242a13a9
unknown <sanchit2005yudavanshi@gmail.com>
2024-05-11 17:42:33 +0530

    added environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BITLY_TOKE: "2fe7fce229764a09c119bfc3a57d97612f8c6c36",
+    GROUP_ID: "Bo5afALkuSl",
+  },
+};
 
 export default nextConfig;
