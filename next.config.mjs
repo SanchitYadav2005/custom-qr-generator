191310c591c6a6bd4e66e25050c1c306a47a374f
unknown <sanchit2005yudavanshi@gmail.com>
2024-06-01 18:06:51 +0530

    updated the name of the token

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BITLY_TOKE: "2fe7fce229764a09c119bfc3a57d97612f8c6c36",
+    BITLY_TOKEN: "2fe7fce229764a09c119bfc3a57d97612f8c6c36",
     GROUP_ID: "Bo5afALkuSl",
   },
 };
