ca76eba0b81139d08918dfc8cf12dab5ec23c507
unknown <sanchit2005yudavanshi@gmail.com>
2024-06-03 13:43:36 +0530

    removed env info from next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    BITLY_TOKEN: "2fe7fce229764a09c119bfc3a57d97612f8c6c36",
-    GROUP_ID: "Bo5afALkuSl",
-  },
+ 
 };
 
 export default nextConfig;
